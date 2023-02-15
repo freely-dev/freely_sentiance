5ec6b5d256488459caf3b4a2b6a15fe8595f35e5
Vivek Rathod <vivek@freely.me>
2023-02-15 20:15:35 +1100

    updates

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 //test
-
+//develop is ahead of master
 AppRegistry.registerComponent(appName, () => App);
